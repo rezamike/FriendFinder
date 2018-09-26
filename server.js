1971be0e396e321375893631b7b6fd230bd69e2b
rezamike <michael.sanaiha@gmail.com>
2018-09-26 15:17:25 -0700

    i'm essentially lying to myself at this point

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+let inquirer = require("inquirer");
+let request = require("request");
+let mysql = require("mysql");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8083;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
